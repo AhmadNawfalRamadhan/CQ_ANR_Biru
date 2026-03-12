0429040cff3e07a29644deebbfc61045e6c1e55a
AhmadNawfalRamadhan <Ahmadnawfalramadhan@gmail.com>
2026-03-12 11:36:10 +0700

    update dan fungsi nya

--- PraktikumADO/Form1.Designer.cs
+++ PraktikumADO/Form1.Designer.cs
@@ -36,6 +36,7 @@
             this.txtHasil = new System.Windows.Forms.TextBox();
             this.btnHitungdosen = new System.Windows.Forms.Button();
             this.btnUpdateMk = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnConnect
@@ -115,11 +116,21 @@
             this.btnUpdateMk.UseVisualStyleBackColor = true;
             this.btnUpdateMk.Click += new System.EventHandler(this.btnUpdateMk_Click);
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(382, 332);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.btnUpdateMk);
             this.Controls.Add(this.btnHitungdosen);
             this.Controls.Add(this.txtHasil);
@@ -145,6 +156,7 @@
         private System.Windows.Forms.TextBox txtHasil;
         private System.Windows.Forms.Button btnHitungdosen;
         private System.Windows.Forms.Button btnUpdateMk;
+        private System.Windows.Forms.Button button1;
     }
 }
 
