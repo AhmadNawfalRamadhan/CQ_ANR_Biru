b39eba744462b81920ced9435c7a08b7c56ec082
AhmadNawfalRamadhan <Ahmadnawfalramadhan@gmail.com>
2026-03-12 10:21:35 +0700

    Add project files.

---
+++ PraktikumADO/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace PraktikumADO
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection conn;
+        SqlCommand cmd;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Koneksi()
+        {
+            conn = new SqlConnection(
+                "Data Source=DESKTOP-9K7QG8P;Initial Catalog=PraktikumADO;Integrated Security=True");
+        }
+    }
+}
