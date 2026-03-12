d9b4f4721579a2b75a9e57611258b6f640818e95
AhmadNawfalRamadhan <Ahmadnawfalramadhan@gmail.com>
2026-03-12 10:52:37 +0700

    menambahkan function di class btnHitungMk_Click

--- PraktikumADO/Form1.cs
+++ PraktikumADO/Form1.cs
@@ -67,7 +67,20 @@ namespace PraktikumADO
         }
         private void btnHitungMk_Click(object sender, EventArgs e)
         {
+            try
+            {
+                Koneksi();
+                conn.Open();
 
+                string query = "SELECT COUNT(*) FROM MataKuliah";
+
+                cmd = new SqlCommand(query, conn);
+
+                int jumlah = (int)cmd.ExecuteScalar();
+
+                txtHasil.Text = jumlah.ToString();  
+                conn.Close();
+            }
         }
     }
 }
