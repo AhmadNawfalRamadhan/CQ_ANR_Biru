176bb260c09b1ef6c062dc40dd1096dfd313076e
AhmadNawfalRamadhan <Ahmadnawfalramadhan@gmail.com>
2026-03-12 11:03:50 +0700

    menambah message box berhasil di class btnUpdate_click

--- PraktikumADO/Form1.cs
+++ PraktikumADO/Form1.cs
@@ -94,8 +94,14 @@ namespace PraktikumADO
                 conn.Open();
 
                 string query = "UPDATE Mahasiswa SET Alamat = 'Yogyakarta' WHERE NIM = '23110100001'";
+
                 cmd = new SqlCommand(query, conn);
+
                 int hasil = cmd.ExecuteNonQuery();
+
+                MessageBox.Show("Jumlah baris terpengaruh" + hasil);
+
+                conn.Close();
 
             }
         }
